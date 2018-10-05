f73a181feecc9a2d08d8f896ee397ca7decbc52c
chang-chao <chang-chao@hotmail.com>
2018-10-06 01:00:20 +0900

    Polish.

--- src/main/java/me/changchao/webflux/reactivecats/service/impl/CatServiceImpl.java
+++ src/main/java/me/changchao/webflux/reactivecats/service/impl/CatServiceImpl.java
@@ -19,11 +19,11 @@ import reactor.core.publisher.Signal;
 
 @Service
 public class CatServiceImpl implements CatService {
+	private static final String CACHE_NAME = "sr";
+	private static final String KEY = "k";
+
 	@Autowired
 	private CacheManager cacheManager;
-
-	private static String cacheName = "search_result";
-	private static String key = "search_result_key";
 
 	@Override
 	public Flux<CatDto> search() {
@@ -31,19 +31,19 @@ public class CatServiceImpl implements CatService {
 		WebClient client = WebClient.create("https://api.thecatapi.com/v1/images/search");
 		Flux<CatDto> fromServer = client.get().retrieve().bodyToFlux(CatDto.class);
 
-		return CacheFlux.lookup(reader(), key).onCacheMissResume(fromServer).andWriteWith(writer());
+		return CacheFlux.lookup(reader(), KEY).onCacheMissResume(fromServer).andWriteWith(writer());
 	}
 
 	private BiFunction<String, List<Signal<CatDto>>, Mono<Void>> writer() {
 		return (k, sigs) -> Flux.fromIterable(sigs).dematerialize().collectList()
-				.doOnNext(l -> cacheManager.getCache(cacheName).put(k, l)).then();
+				.doOnNext(l -> cacheManager.getCache(CACHE_NAME).put(k, l)).then();
 	}
 
 	@SuppressWarnings("unchecked")
 	private Function<String, Mono<List<Signal<CatDto>>>> reader() {
 		return k -> Mono
 				.justOrEmpty(
-						(Optional.ofNullable((List<CatDto>) (cacheManager.getCache(cacheName).get(k, List.class)))))
+						(Optional.ofNullable((List<CatDto>) (cacheManager.getCache(CACHE_NAME).get(k, List.class)))))
 				.flatMap(v -> Flux.fromIterable(v).materialize().collectList());
 	}
 

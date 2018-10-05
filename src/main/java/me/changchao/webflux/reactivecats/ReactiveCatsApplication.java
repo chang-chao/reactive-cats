f39ee1f8bc2b334d8f96ff2a0ec0a94540d2917b
chao.chang <chao.chang@linecorp.com>
2018-10-05 17:47:29 +0900

    first commit

---
+++ src/main/java/me/changchao/webflux/reactivecats/ReactiveCatsApplication.java
@@ -0,0 +1,12 @@
+package me.changchao.webflux.reactivecats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveCatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveCatsApplication.class, args);
+	}
+}

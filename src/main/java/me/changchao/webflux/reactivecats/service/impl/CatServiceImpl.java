8b740fc4b4403236cbc0890d40e8840bedddf370
chang-chao <chang-chao@hotmail.com>
2018-10-06 13:38:39 +0900

    Change reader and writer to member variable.

--- src/main/java/me/changchao/webflux/reactivecats/service/impl/CatServiceImpl.java
+++ src/main/java/me/changchao/webflux/reactivecats/service/impl/CatServiceImpl.java
@@ -25,26 +25,21 @@ public class CatServiceImpl implements CatService {
 	@Autowired
 	private CacheManager cacheManager;
 
+	@SuppressWarnings("unchecked")
+	private Function<String, Mono<List<Signal<CatDto>>>> reader = k -> Mono
+			.justOrEmpty((Optional.ofNullable((List<CatDto>) (cacheManager.getCache(CACHE_NAME).get(k, List.class)))))
+			.flatMap(v -> Flux.fromIterable(v).materialize().collectList());
+
+	private BiFunction<String, List<Signal<CatDto>>, Mono<Void>> writer = (k, sigs) -> Flux.fromIterable(sigs)
+			.dematerialize().collectList().doOnNext(l -> cacheManager.getCache(CACHE_NAME).put(k, l)).then();
+
 	@Override
 	public Flux<CatDto> search() {
 		// https://stackoverflow.com/questions/49095366/right-way-to-use-spring-webclient-in-multi-thread-environment
 		WebClient client = WebClient.create("https://api.thecatapi.com/v1/images/search");
 		Flux<CatDto> fromServer = client.get().retrieve().bodyToFlux(CatDto.class);
 
-		return CacheFlux.lookup(reader(), KEY).onCacheMissResume(fromServer).andWriteWith(writer());
-	}
-
-	private BiFunction<String, List<Signal<CatDto>>, Mono<Void>> writer() {
-		return (k, sigs) -> Flux.fromIterable(sigs).dematerialize().collectList()
-				.doOnNext(l -> cacheManager.getCache(CACHE_NAME).put(k, l)).then();
-	}
-
-	@SuppressWarnings("unchecked")
-	private Function<String, Mono<List<Signal<CatDto>>>> reader() {
-		return k -> Mono
-				.justOrEmpty(
-						(Optional.ofNullable((List<CatDto>) (cacheManager.getCache(CACHE_NAME).get(k, List.class)))))
-				.flatMap(v -> Flux.fromIterable(v).materialize().collectList());
+		return CacheFlux.lookup(reader, KEY).onCacheMissResume(fromServer).andWriteWith(writer);
 	}
 
 }
